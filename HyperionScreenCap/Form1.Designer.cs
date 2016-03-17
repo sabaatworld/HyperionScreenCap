7fe1f883f8c7246142072e1e478499179b447e61
Hansel <hansel@autoloop.com>
2016-03-17 11:56:47 -0400

    Remove trayIcon on exit and changed build config.

--- HyperionScreenCap/Form1.Designer.cs
+++ HyperionScreenCap/Form1.Designer.cs
@@ -15,6 +15,7 @@
         {
             if (disposing && (components != null))
             {
+                trayIcon.Dispose();
                 closeConnection();
                 components.Dispose();
             }
