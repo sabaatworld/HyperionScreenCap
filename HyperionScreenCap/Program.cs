643636cd071d43ef5ee8030e3c9c524c1c9bc74b
Hansel <hansel@autoloop.com>
2016-03-22 15:55:44 -0400

    Binaries and version change

--- HyperionScreenCap/Program.cs
+++ HyperionScreenCap/Program.cs
@@ -14,24 +14,6 @@ namespace HyperionScreenCap
         [STAThread]
         static void Main()
         {
-            #region Use Embedded SlimDX Assembly
-
-            AppDomain.CurrentDomain.AssemblyResolve += (Object sender, ResolveEventArgs args) =>
-            {
-                String thisExe = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-                System.Reflection.AssemblyName embeddedAssembly = new System.Reflection.AssemblyName(args.Name);
-                String resourceName = thisExe + "." + embeddedAssembly.Name + ".dll";
-
-                using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-                {
-                    Byte[] assemblyData = new Byte[stream.Length];
-                    stream.Read(assemblyData, 0, assemblyData.Length);
-                    return System.Reflection.Assembly.Load(assemblyData);
-                }
-            };
-
-            #endregion
-
             mainForm = new Form1();
             Application.Run(mainForm);
             
